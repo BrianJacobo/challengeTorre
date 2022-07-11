b53a46cf345a2c2f7683580308ee3451ba8cc375
BrianJacobo <jacobo.brian.fuentes@outlook.com>
2022-07-11 11:35:42 -0500

    testing some services

--- src/api-github/github.service.ts
+++ src/api-github/github.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+// import { HttpService } from '@nestjs/axios';
+import * as fs from 'fs';
+import csv from 'csv-parser';
+import path from 'path';
 
 @Injectable()
-export class GithubService {}
+export class GithubService {
+  // constructor(private readonly http: HttpService) {}
+
+  async getData() {
+    const finalPath = path.join(__dirname, 'files', 'ranking.csv');
+    console.log(finalPath);
+    const results: Array<any> = [];
+    fs.createReadStream(finalPath)
+      .pipe(csv())
+      .on('data', (data) => results.push(data))
+      .on('end', () => {
+        results.map((item) => {
+          console.log('==>', item.stars);
+        });
+        // console.log(results);
+      });
+  }
+}
