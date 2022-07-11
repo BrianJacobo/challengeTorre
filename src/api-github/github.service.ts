451b8cecb2ee629d8246d4d5903a7301f7939188
BrianJacobo <jacobo.brian.fuentes@outlook.com>
2022-07-11 12:52:18 -0500

    test service finished

--- src/api-github/github.service.ts
+++ src/api-github/github.service.ts
@@ -2,24 +2,29 @@ import { Injectable } from '@nestjs/common';
 // import { HttpService } from '@nestjs/axios';
 import * as fs from 'fs';
 import csv from 'csv-parser';
-import path from 'path';
 
 @Injectable()
 export class GithubService {
-  // constructor(private readonly http: HttpService) {}
+  private arrayFiltered: Array<any> = [];
 
   async getData() {
-    const finalPath = path.join(__dirname, 'files', 'ranking.csv');
-    console.log(finalPath);
     const results: Array<any> = [];
-    fs.createReadStream(finalPath)
+    fs.createReadStream('ranking.csv')
       .pipe(csv())
       .on('data', (data) => results.push(data))
       .on('end', () => {
-        results.map((item) => {
-          console.log('==>', item.stars);
-        });
-        // console.log(results);
+        console.log(results.length);
+        results.sort((a, b) => b.stars - a.stars);
+        const newResults = results.filter(
+          (item) => item.language === 'JavaScript',
+        );
+        for (let i = 0; i < 2; i++) {
+          this.arrayFiltered.push(newResults[i]);
+        }
+        console.log('=====>', this.arrayFiltered);
+        var res = this.arrayFiltered;
       });
+    console.log(this.arrayFiltered);
+    return res;
   }
 }
